1361f3ca3918da0f2df4cd275492e40f1966036e
pooya <pooya9685@gmail.com>
2025-11-02 19:52:39 -0500

    fix: change baseURL

--- src/services/api/productApi.ts
+++ src/services/api/productApi.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 const api = axios.create({
-    baseURL: 'https://crm-app-six-ruby.vercel.app/api/',
+    baseURL: 'https://react-shop-brown-nine.vercel.app//api/',
 });
 export const productApi = async (endpoint:string) => {
     const response = await api.get(endpoint);
